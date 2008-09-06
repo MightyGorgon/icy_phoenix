0e007c0e86b911795d25d7d4f24ca3cc5e2c0af3
Luca Libralato <mightygorgon@mightygorgon.com>
2008-09-06 09:40:52 +0000

    Improved URL rewrite
    git-svn-id: http://svn.assembla.com/svn/icy_phoenix/ip/trunk@47 e0e46c49-be69-4f5a-ad62-21024a331aea

--- includes/functions_rewrite.php
+++ includes/functions_rewrite.php
@@ -16,11 +16,10 @@ if (!defined('IN_PHPBB'))
 // Make URL Friendly rewritten by MG
 function make_url_friendly($url)
 {
-
-	$url = strtolower($url);
+	$url = strtolower(str_replace('Re: ', '', strip_tags($url)));
 
 	$find = array(
-		' ', '<b>', '</b>',
+		' ',
 		'&quot;', '&amp;', '&lt;', '&gt;', '\r\n', '\n', '/', '\\', '+', '<', '>', 'vfr',
 		'á', 'à', 'â', 'ã', 'å', 'Á', 'À', 'Â', 'Ã', 'Å', 'ä', 'Ä',
 		'é', 'è', 'ë', 'ê', 'É', 'È', 'Ê', 'Ë',
@@ -36,7 +35,7 @@ function make_url_friendly($url)
 	);
 
 	$repl = array(
-		'-', '', '',
+		'-',
 		'-', '-', '-', '-', '-', '-', '-', '-', '-', '-',
 		'a', 'a', 'a', 'a', 'a', 'A', 'A', 'A', 'A', 'A', 'ae', 'ae',
 		'e', 'e', 'e', 'e', 'E', 'E', 'E', 'E',
@@ -83,16 +82,29 @@ function rewrite_urls($content)
 		//'/(?<!\/)' . PORTAL_MG . '\?topic_id=([0-9]+)((&amp;)|(&)){0,1}([^>]+>)(.*?)<\/a>/e',
 		'/(?<!\/)' . FORUM_MG . '\?' . POST_CAT_URL . '=([0-9]+)((&amp;)|(&)){0,1}([^>]+>)(.*?)<\/a>/e',
 		'/(?<!\/)' . VIEWFORUM_MG . '\?' . POST_FORUM_URL . '=([0-9]+)((&amp;)|(&)){0,1}([^>]+>)(.*?)<\/a>/e',
+		'/(?<!\/)' . VIEWTOPIC_MG . '\?' . POST_FORUM_URL . '=([0-9]+)((&amp;)|(&))' . POST_TOPIC_URL . '=([0-9]+)((&amp;)|(&))' . POST_POST_URL . '=([0-9]+)((&amp;)|(&)){0,1}([^>]+>)(.*?)alt=\"(.*?)\"(.*?)<\/a>/e',
+		'/(?<!\/)' . VIEWTOPIC_MG . '\?' . POST_FORUM_URL . '=([0-9]+)((&amp;)|(&))' . POST_TOPIC_URL . '=([0-9]+)((&amp;)|(&))' . POST_POST_URL . '=([0-9]+)((&amp;)|(&)){0,1}([^>]+>)(.*?)<\/a>/e',
+		'/(?<!\/)' . VIEWTOPIC_MG . '\?' . POST_FORUM_URL . '=([0-9]+)((&amp;)|(&))' . POST_POST_URL . '=([0-9]+)((&amp;)|(&)){0,1}([^>]+>)(.*?)alt=\"(.*?)\"(.*?)<\/a>/e',
+		'/(?<!\/)' . VIEWTOPIC_MG . '\?' . POST_FORUM_URL . '=([0-9]+)((&amp;)|(&))' . POST_TOPIC_URL . '=([0-9]+)((&amp;)|(&)){0,1}([^>]+>)(.*?)alt=\"(.*?)\"(.*?)<\/a>/e',
+		'/(?<!\/)' . VIEWTOPIC_MG . '\?' . POST_FORUM_URL . '=([0-9]+)((&amp;)|(&))' . POST_POST_URL . '=([0-9]+)((&amp;)|(&)){0,1}([^>]+>)(.*?)<\/a>/e',
+		'/(?<!\/)' . VIEWTOPIC_MG . '\?' . POST_FORUM_URL . '=([0-9]+)((&amp;)|(&))' . POST_TOPIC_URL . '=([0-9]+)((&amp;)|(&)){0,1}([^>]+>)(.*?)<\/a>/e',
 		'/(?<!\/)' . VIEWTOPIC_MG . '\?' . POST_TOPIC_URL . '=([0-9]+)((&amp;)|(&)){0,1}([^>]+>)(.*?)<\/a>/e',
 		'/(?<!\/)' . VIEWTOPIC_MG . '\?' . POST_POST_URL . '=([0-9]+)((&amp;)|(&)){0,1}([^>]+>)(.*?)<\/a>/e',
 		'/(?<!\/)album_cat.php\?cat_id=([0-9]+)((&amp;)|(&)){0,1}([^>]+>)(.*?)<\/a>/e',
-		'/(?<!\/)album_showpage.php\?pic_id=([0-9]+)((&amp;)|(&)){0,1}([^>]+>)(.*?)<\/a>/e',
+		//'/(?<!\/)album_showpage.php\?pic_id=([0-9]+)((&amp;)|(&)){0,1}([^>]+>)(.*?)<\/a>/e',
+		// replacing pic_id with IMG ALT content
+		'/(?<!\/)album_showpage.php\?pic_id=([0-9]+)((&amp;)|(&)){0,1}([^>]+>)(.*?)alt=\"(.*?)\"(.*?)<\/a>/e',
 		'/(?<!\/)album_personal.php\?user_id=([0-9]+)((&amp;)|(&)){0,1}([^>]+>)(.*?)<\/a>/e',
-		/* */
-		'/(?<!\/)album_pic.php\?pic_id=([0-9]+)((&amp;)|(&))full=true{0,1}([^>]+>)(.*?)<\/a>/e',
+		/*
 		'/(?<!\/)album_pic.php\?pic_id=([0-9]+)((&amp;)|(&)){0,1}([^>]+>)(.*?)<\/a>/e',
 		'/(?<!\/)album_picm.php\?pic_id=([0-9]+)((&amp;)|(&)){0,1}([^>]+>)(.*?)<\/a>/e',
 		'/(?<!\/)album_thumbnail.php\?pic_id=([0-9]+)((&amp;)|(&)){0,1}([^>]+>)(.*?)<\/a>/e',
+		*/
+		/* */
+		// replacing pic_id with IMG ALT content
+		'/(?<!\/)album_pic.php\?pic_id=([0-9]+)((&amp;)|(&)){0,1}([^>]+alt=\")(.*?)\"/e',
+		'/(?<!\/)album_picm.php\?pic_id=([0-9]+)((&amp;)|(&)){0,1}([^>]+alt=\")(.*?)\"/e',
+		'/(?<!\/)album_thumbnail.php\?pic_id=([0-9]+)((&amp;)|(&)){0,1}([^>]+alt=\")(.*?)\"/e',
 		/* */
 		'/(?<!\/)dload.php\?action=category&cat_id=([0-9]+)((&amp;)|(&)){0,1}([^>]+>)(.*?)<\/a>/e',
 		'/(?<!\/)dload.php\?action=category&cat_id=([0-9]+)((&amp;)|(&)){0,1}([^>]+>)(.*?)<\/a>/e',
@@ -108,16 +120,29 @@ function rewrite_urls($content)
 		//"make_url_friendly('\\6') . '-na\\1.html' . if_query('\\2') . stripslashes('\\5\\6') . '</a>'",
 		"make_url_friendly('\\6') . '-vc\\1.html' . if_query('\\2') . stripslashes('\\5\\6') . '</a>'",
 		"make_url_friendly('\\6') . '-vf\\1.html' . if_query('\\2') . stripslashes('\\5\\6') . '</a>'",
+		"make_url_friendly('\\15') . '-vf\\1-vt\\5-vp\\9.html' . if_query('\\11') . stripslashes('\\13\\14') . 'alt=\"' . stripslashes('\\15') . '\"' . stripslashes('\\16') . '</a>'",
+		"make_url_friendly('\\14') . '-vf\\1-vt\\5-vp\\9.html' . if_query('\\10') . stripslashes('\\13\\14') . '</a>'",
+		"make_url_friendly('\\11') . '-vf\\1-vp\\5.html' . if_query('\\6') . stripslashes('\\9\\10') . 'alt=\"' . stripslashes('\\11') . '\"' . stripslashes('\\12') . '</a>'",
+		"make_url_friendly('\\11') . '-vf\\1-vt\\5.html' . if_query('\\6') . stripslashes('\\9\\10') . 'alt=\"' . stripslashes('\\11') . '\"' . stripslashes('\\12') . '</a>'",
+		"make_url_friendly('\\10') . '-vf\\1-vp\\5.html' . if_query('\\6') . stripslashes('\\9\\10') . '</a>'",
+		"make_url_friendly('\\10') . '-vf\\1-vt\\5.html' . if_query('\\6') . stripslashes('\\9\\10') . '</a>'",
 		"make_url_friendly('\\6') . '-vt\\1.html' . if_query('\\2') . stripslashes('\\5\\6') . '</a>'",
 		"make_url_friendly('\\6') . '-vp\\1.html' . if_query('\\2') . stripslashes('\\5\\6') . '</a>'",
 		"make_url_friendly('\\6') . '-ac\\1.html' . if_query('\\2') . stripslashes('\\5\\6') . '</a>'",
-		"make_url_friendly('\\6') . '-asp\\1.html' . if_query('\\2') . stripslashes('\\5\\6') . '</a>'",
+		//"make_url_friendly('\\6') . '-asp\\1.html' . if_query('\\2') . stripslashes('\\5\\6') . '</a>'",
+		// replacing pic_id with IMG ALT content
+		"make_url_friendly('\\7') . '-asp\\1.html' . if_query('\\2') . stripslashes('\\5\\6') . 'alt=\"' . stripslashes('\\7') . '\"' . stripslashes('\\8') . '</a>'",
 		"make_url_friendly('\\6') . '-aper\\1.html' . if_query('\\2') . stripslashes('\\5\\6') . '</a>'",
+		/*
+		"make_url_friendly('\\6') . '-apic\\1.jpg' . if_query('\\2') . stripslashes('\\5\\6') . '</a>'",
+		"make_url_friendly('\\6') . '-apm\\1.jpg' . if_query('\\2') . stripslashes('\\5\\6') . '</a>'",
+		"make_url_friendly('\\6') . '-at\\1.jpg' . if_query('\\2') . stripslashes('\\5\\6') . '</a>'",
+		*/
 		/* */
-		"make_url_friendly('\\6') . '-apicf\\1.html' . if_query('\\2') . stripslashes('\\5\\6') . '</a>'",
-		"make_url_friendly('\\6') . '-apic\\1.html' . if_query('\\2') . stripslashes('\\5\\6') . '</a>'",
-		"make_url_friendly('\\6') . '-apm\\1.html' . if_query('\\2') . stripslashes('\\5\\6') . '</a>'",
-		"make_url_friendly('\\6') . '-at\\1.html' . if_query('\\2') . stripslashes('\\5\\6') . '</a>'",
+		// replacing pic_id with IMG ALT content
+		"make_url_friendly('\\6') . '-apic\\1.jpg' . '\" alt=\"' . stripslashes('\\6') . '\"'",
+		"make_url_friendly('\\6') . '-apm\\1.jpg' . '\" alt=\"' . stripslashes('\\6') . '\"'",
+		"make_url_friendly('\\6') . '-at\\1.jpg' . '\" alt=\"' . stripslashes('\\6') . '\"'",
 		/* */
 		"make_url_friendly('\\6') . '-dc\\1.html' . if_query('\\2') . stripslashes('\\5\\6') . '</a>'",
 		"make_url_friendly('\\6') . '-dc\\1.html' . if_query('\\2') . stripslashes('\\5\\6') . '</a>'",
