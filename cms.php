6f90a9d2c86d431fada90739f01472f504a450df
Luca Libralato <mightygorgon@mightygorgon.com>
2010-09-12 21:34:13 +0000

    GIT sucks
    git-svn-id: http://svn.assembla.com/svn/icy_phoenix/ip/trunk@244 e0e46c49-be69-4f5a-ad62-21024a331aea

--- cms.php
+++ cms.php
@@ -2,7 +2,7 @@
 /**
 *
 * @package Icy Phoenix
-* @version $Id: cms.php 171 2010-02-07 18:29:35Z Mighty Gorgon $
+* @version $Id$
 * @copyright (c) 2008 Icy Phoenix
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
 *
