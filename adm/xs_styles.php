eafdd211663831d23c8d58f4dc7a8639f80178df
John leach <jhl-verona@gmail.com>
2012-04-12 09:20:04 +0100

    Fixed zero value parameter problem - could not switch off the flag

--- adm/xs_styles.php
+++ adm/xs_styles.php
@@ -34,8 +34,8 @@ if(!empty($setdefault) && !defined('DEMO_MODE'))
 }
 
 // change "override" variable
-$setoverride = request_var('setoverride', 0);
-if(!empty($setoverride) && !defined('DEMO_MODE'))
+$setoverride = request_var('setoverride', $config['override_user_style']);
+if(($setoverride != $config['override_user_style']) && !defined('DEMO_MODE'))
 {
 	set_config('override_user_style', $setoverride, false);
 }
