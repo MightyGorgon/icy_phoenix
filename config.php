3511e8fae29e353675fa698c0382cf8cb6457558
Hans Fase <hans@hfase.com>
2012-04-21 04:30:06 -0400

    Installed IcyPhoenix, please do not pull this commit!

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+// Icy Phoenix auto-generated config file
+// Do not change anything in this file!
+
+$dbms = 'mysql4';
+
+$dbhost = '127.0.0.1';
+$dbname = 'icyphoenix';
+$dbuser = 'icyuser';
+$dbpasswd = 'GW5VsBc5YWyXwb7K';
+
+$table_prefix = 'ip_';
+
+define('IP_INSTALLED', true);
+
+?>
